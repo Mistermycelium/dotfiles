115c07408a9681eb86a4bc135519dadd3f7e5dbd
Beeheim <beeheimishappy@gmail.com>
2024-02-16 21:38:04 +0100

    y

---
+++ dot_config/nvim/lua/plugins/plugins.lua
@@ -0,0 +1,10 @@
+-- all plugins to be loaded by lazy.nvim
+-- see plugins.lua for details
+
+return {
+  -- ThePrimeagen/vim-be-good
+  {
+    "ThePrimeagen/vim-be-good",
+    lazy = false,
+  },
+}
