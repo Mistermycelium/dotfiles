8e60f22e1cb557ac76a4265c827131c0cb2de637
Cody Smith <csmith.su@gmail.com>
2024-02-17 23:52:45 -0800

    Update .config/nvim/lua/config/keymaps.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Map <leader>Pt to telescope builtins using recordings
+vim.keymap.set("n", "<leader>Pt", function()
+  require("telescope.builtin").builtin()
+end, { noremap = true, silent = true, desc = "Telescope" })
