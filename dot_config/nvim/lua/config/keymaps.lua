e3763bc2869e868cf0dbe56edf7b50f14d9f62c6
Cody Smith <csmith.su@gmail.com>
2024-02-18 00:24:58 -0800

    Update .config/nvim/lua/config/keymaps.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,27 @@
 vim.keymap.set("n", "<leader>Pt", function()
   require("telescope.builtin").builtin()
 end, { noremap = true, silent = true, desc = "Telescope" })
+
+-- remap <C-d> and <C-u> to scroll the view
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Keep cursor in place when joining lines
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Keep search terms in the middle of the screen
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search term in the middle of the screen" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Prev search term in the middle of the screen" })
+
+-- Paste without replacing clipboard
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste without replacing clipboard" })
+
+-- System copy
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "System copy" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "System copy line" })
+
+-- Delete without replacing clipboard
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete without replacing clipboard" })
+
+-- Map <leader>r to replace word under cursor
+vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
