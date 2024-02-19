c04c356907c7e5b4a524dfa0e53a5475f34e6c23
Cody Smith <csmith.su@gmail.com>
2024-02-18 20:36:45 -0800

    Add .config/nvim/lua/plugins/distant.lua Add .config/nvim/lua/plugins/neo-tree.lua Add .config/nvim/lua/plugins/small-plugins.lua

---
+++ dot_config/nvim/lua/plugins/distant.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "chipsenkbeil/distant.nvim",
+    branch = "v0.3",
+    config = function()
+      require("distant"):setup({
+        ["network.private"] = true,
+        manager = {
+          log_file = "$HOME/.distant.log",
+        },
+        servers = {
+          ["*"] = {
+            connect = {
+              default = {
+                scheme = "ssh",
+              },
+            },
+          },
+          ["beeheim.buzz"] = {
+            -- Change the current working directory and specify
+            -- a path to the distant binary on the remote machine
+            port = 4022,
+            cwd = "~/",
+            username = "beeheim",
+            key = "id_rsa",
+            launch = {
+              bin = "/path/to/distant",
+            },
+          },
+        },
+      })
+    end,
+  },
+}
---
+++ dot_config/nvim/lua/plugins/small-plugins.lua
@@ -0,0 +1,11 @@
+-- all plugins to be loaded by lazy.nvim
+-- see plugins.lua for details
+
+return {
+  -- ThePrimeagen/vim-be-good
+  {
+    "ThePrimeagen/vim-be-good",
+    lazy = false,
+    priority = 1000,
+  },
+}
